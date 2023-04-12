54b936497d6cd70dba74e72c2c311221454dc084
ksfi <kerian.yousfi@hotmail.fr>
2023-04-12 02:46:49 +0200

    adding dms

---
+++ dm.py
@@ -0,0 +1,56 @@
+import hashlib
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+from message import *
+
+class AESCipher:
+    def __init__(self, key):
+        self.bs = AES.block_size
+        self.key = hashlib.sha256(key).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw.encode())), iv
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+class DM(Message):
+    def __init__(self, relay, privkey, pubkey, message, otherPubkey):
+        super().__init__(relay, privkey, pubkey, message, otherPubkey)
+        self.otherPubkey = self.from_npub(otherPubkey)
+        print(type(self.otherPubkey))
+        self.aes = AESCipher(self.otherPubkey)
+
+    def communication(self):
+        while (1):
+            ipt = input("-> ")
+            if ipt == 'q':
+                break
+            message, iv = self.aes.encrypt(ipt)
+            asyncio.run(self.sendNote(message, iv))
+
+if __name__ == "__main__":
+    PRIVKEY = secrets.token_bytes(32)
+    sk = secp256k1.PrivateKey(PRIVKEY)
+    PUBKEY = sk.pubkey.serialize()[1:]
+    RELAY = "wss://nostr.massmux.com"
+    data = "oui c un dm".encode('utf-8')
+
+#     cipher = AES.new(PUBKEY, AES.MODE_CBC)
+#     ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+#     iv = base64.b64encode(cipher.iv).decode('utf-8')
+#     ct = base64.b64encode(ct_bytes).decode('utf-8')
+#     print(ct)
+
+    aes = AESCipher(PUBKEY)
+    dm = DM(RELAY, PRIVKEY, PUBKEY, '', "npub1prxd2w0eunppgfwnr9ppdx39dmcee2ltzra3n8c9gqgaqn56zz9snzex3w")
+    dm.communication()
+
--- message.py
+++ message.py
@@ -7,11 +7,11 @@ import secp256k1
 import bech32
 import secrets
 import os
-import datetime
+import base64
 
 # message = note
 class Message:
-    def __init__(self, relay, privkey, pubkey, message):
+    def __init__(self, relay, privkey, pubkey, message, otherPubkey=None):
         if privkey[:4] == "nsec" and pubkey[:4] == "npub":
             self.keytype = "nsec"
             self.privkey = privkey
@@ -57,7 +57,7 @@ class Message:
         sig = sk.schnorr_sign(hash, None, raw=True)
         return sig.hex()
 
-    def get_id(self):
+    def get_id(self, dm_message, iv):
         if self.keytype == "nsec":
             privk = self.from_nsec().hex()
             pubk = self.from_npub().hex()
@@ -67,19 +67,28 @@ class Message:
         else:
             privk = self.privkey
             pubk = self.pubkey
+        if dm_message is not None:
+            kind = 4
+            tags = [["p", str(self.otherPubkey.hex())]]
+            content = str(base64.b64encode(self.message).decode('utf-8')) + "?iv=" + str(base64.b64encode(iv).decode('utf-8'))
+        else:
+            kind = 1
+            tags = []
+            content = self.message
+        print("content:", content)
         event = [
           0,
           pubk,
           int(time.time()),
-          1,
-          [],
-          self.message
+          kind,
+          tags,
+          content
         ]
         event_json = json.dumps(event, separators=(',', ':'), ensure_ascii=False)
         event_hash = hashlib.sha256(event_json.encode('utf-8')).digest()
         return event_hash
 
-    def to_json(self):
+    def to_json(self, dm_message, iv):
         if self.keytype == "nsec":
             privk = self.from_nsec().hex()
             pubk = self.from_npub().hex()
@@ -89,21 +98,34 @@ class Message:
         else:
             privk = self.privkey
             pubk = self.pubkey
+        if dm_message is not None:
+            kind = 4
+            self.message = dm_message
+            tags = [["p", str(self.otherPubkey.hex())]]
+            content = str(base64.b64encode(self.message).decode('utf-8')) + "?iv=" + str(base64.b64encode(iv).decode('utf-8'))
+        else:
+            content = self.message
+            kind = 1
+            tags = []
+        print(self.message)
+        print(base64.b64encode(self.message).decode('utf-8'))
         ret = {
-          "id": self.get_id().hex(),
+          "id": self.get_id(dm_message, iv).hex(),
           "pubkey": pubk,
           "created_at": int(time.time()),
-          "kind": 1,
-          "tags": [],
-          "content": self.message,
-          "sig": self.sign_message_hash(self.get_id())
+          "kind": kind,
+          "tags": tags,
+          "content": content,
+          "sig": self.sign_message_hash(self.get_id(dm_message, iv))
         }
         return ret
 
-    async def sendNote(self):
-        note = json.dumps(["EVENT", self.to_json()], separators=(',', ':'), ensure_ascii=False)
+    async def sendNote(self, dm_message=None, iv=None):
+        note = json.dumps(["EVENT", self.to_json(dm_message, iv)], separators=(',', ':'), ensure_ascii=False)
         async with websockets.connect(self.relay) as websocket:
             await websocket.send(note)
+            r = await websocket.recv()
+            print(r)
 
     async def receive(self):
         k = 0
@@ -116,44 +138,13 @@ class Message:
                 m = await ws.recv()
                 m = json.loads(m)
                 tags_p = [x[1] for x in m[2]["tags"] if x[0] == 'p']
+                tags_e = [x[1] for x in m[2]["tags"] if x[0] == 'e']
                 print(f"\n-> by {m[2]['id']} at {time.strftime('%H:%M:%S', time.gmtime(m[2]['created_at']))}\n-> {m[2]['content']}")
-                if len(tags_p) > 0:
+                if len(tags_p):
                     print(f"-> to {tags_p}")
+                    print(f"-> to {tags_e}")
                 print("------------\n------------")
                 k += 1
-
-class DM(Message):
-    def __init__(self, relay, privkey, pubkey, skPrivate, message, pubKeyOther):
-        self.theirPubKey = pubKeyOther
-        self.skPrivate = skPrivate
-
-    def getSecret(self):
-        pk = self.from_npub(self.theirPubKey)
-
-    def to_json(self):
-        if self.keytype == "nsec":
-            privk = self.from_nsec().hex()
-            pubk = self.from_npub().hex()
-        elif self.keytype == "bytes":
-            privk = self.privkey.hex()
-            pubk = self.pubkey.hex()
-        else:
-            privk = self.privkey
-            pubk = self.pubkey
-        ret = {
-          "id": self.get_id().hex(),
-          "pubkey": pubk,
-          "created_at": int(time.time()),
-          "kind": 4,
-          "tags": [],
-          "content": self.message,
-          "sig": self.sign_message_hash(self.get_id())
-        }
-        return ret
-
-    def communication(self):
-        pass
-
 def key(ipt):
     if len(ipt) == 1:
         PRIVKEY = secrets.token_bytes(32)
@@ -188,6 +179,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-
-
-
